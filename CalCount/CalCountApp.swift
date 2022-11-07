c43e879723e1b27d4360d71737ca25d6884871b7
Nick Dazell <ndazell@g.hmc.edu>
2022-11-06 22:26:07 -0800

    Initial Commit

---
+++ CalCount/CalCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalCountApp.swift
+//  CalCount
+//
+//  Created by Nick Dazell on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CalCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalCountUITests/CalCountUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CalCountUITestsLaunchTests.swift
+//  CalCountUITests
+//
+//  Created by Nick Dazell on 11/6/22.
+//
+
+import XCTest
+
+final class CalCountUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
