48a5fd4f59c3f333d21b84dc09f59e2c75542971
Nick Dazell <ndazell@g.hmc.edu>
2022-12-16 00:25:02 -0800

    added save button

--- CalCount/CustomViews/Onboarding.swift
+++ CalCount/CustomViews/Onboarding.swift
@@ -14,35 +14,57 @@ struct Onboarding: View {
     @State var fat: Int? = nil
     @State var carb: Int? = nil
     @State var cal: Int? = nil
+    
+    @State var savedProtein: Int? = nil
+    @State var savedFat: Int? = nil
+    @State var savedCarb: Int? = nil
+    @State var savedCal: Int? = nil
+    
     var body: some View {
         ZStack{
             Color.theme.primary.ignoresSafeArea()
             VStack {
                 Text("Enter Macros")
                     .frame(width:200, height:50)
+                    .foregroundColor(Color.theme.accent)
                 HStack {
-                    Text("Protein:")
+                    Text("Protein: ")
+                        .padding()
+                        .foregroundColor(Color.theme.accent)
                     TextField("Enter protein here...", value: $protein, format: .number)
                         .padding()
                         .foregroundColor(Color.theme.accent)
                 }
                 HStack {
-                    Text("Fat:       ")
+                    Text("Fat:         ")
+                        .padding()
+                        .foregroundColor(Color.theme.accent)
                     TextField("Enter fat here...", value: $fat, format: .number)
                         .padding()
                         .foregroundColor(Color.theme.accent)
                 }
                 HStack {
-                    Text("Carbs:  ")
+                    Text("Carbs:    ")
+                        .padding()
+                        .foregroundColor(Color.theme.accent)
                     TextField("Enter carbs here...", value: $carb, format: .number)
                         .padding()
                         .foregroundColor(Color.theme.accent)
                 }
                 HStack {
                     Text("Calories:")
+                        .padding()
+                        .foregroundColor(Color.theme.accent)
                     TextField("Enter calories here...", value: $cal, format: .number)
                         .padding()
                         .foregroundColor(Color.theme.accent)
+                }
+                Button("Save and Close") {
+                    savedProtein = protein
+                    savedFat = fat
+                    savedCarb = carb
+                    savedCal = cal
+                    appState.hasOnboarded = true
                 }
             }
         }
