0a06e8611a8a00db2b04fd6c315ab2adc390126b
Nick Dazell <ndazell@g.hmc.edu>
2022-12-20 01:09:42 -0800

    macros in onboarding save temporarily. hitting daily log button, leaving app, or saving with no values in text fields resets macros to 0...

--- CalCount/CustomViews/Onboarding.swift
+++ CalCount/CustomViews/Onboarding.swift
@@ -10,23 +10,32 @@ import SwiftUI
 
 struct Onboarding: View {
     @EnvironmentObject var appState: AppState
-    @State var protein: Int? = nil
-    @State var fat: Int? = nil
-    @State var carb: Int? = nil
-    @State var cal: Int? = nil
+    @State var protein: Int = 0
+    @State var fat: Int = 0
+    @State var carb: Int = 0
+    @State var cal: Int = 0
     
-    @State var savedProtein: Int? = nil
-    @State var savedFat: Int? = nil
-    @State var savedCarb: Int? = nil
-    @State var savedCal: Int? = nil
+    @State var savedProtein: Int = UserDefaults.standard.integer(forKey: "ProKey")
+    @State var savedFat: Int = UserDefaults.standard.integer(forKey: "FatKey")
+    @State var savedCarb: Int = UserDefaults.standard.integer(forKey: "CarbKey")
+    @State var savedCal: Int = UserDefaults.standard.integer(forKey: "CalKey")
     
-    /*
+    let quantityFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.zeroSymbol = ""
+        return formatter
+    }()
+    
     func saveMacros() {
-        if savedProtein != nil {
-            protein = savedProtein
-        }
+        UserDefaults.standard.set(protein, forKey: "ProKey")
+        UserDefaults.standard.set(fat, forKey: "FatKey")
+        UserDefaults.standard.set(carb, forKey: "CarbKey")
+        UserDefaults.standard.set(cal, forKey: "CalKey")
+        savedProtein = protein
+        savedFat = fat
+        savedCarb = carb
+        savedCal = cal
     }
-    */
     
     var body: some View {
         ZStack{
@@ -39,7 +48,7 @@ struct Onboarding: View {
                     Text("Protein: ")
                         .padding()
                         .foregroundColor(Color.theme.accent)
-                    TextField("Enter protein here...", value: $protein, format: .number)
+                    TextField("Enter protein here...", value: $protein, formatter: quantityFormatter)
                         .padding()
                         .foregroundColor(Color.theme.accent)
                 }
@@ -47,7 +56,7 @@ struct Onboarding: View {
                     Text("Fat:         ")
                         .padding()
                         .foregroundColor(Color.theme.accent)
-                    TextField("Enter fat here...", value: $fat, format: .number)
+                    TextField("Enter fat here...", value: $fat, formatter: quantityFormatter)
                         .padding()
                         .foregroundColor(Color.theme.accent)
                 }
@@ -55,7 +64,7 @@ struct Onboarding: View {
                     Text("Carbs:    ")
                         .padding()
                         .foregroundColor(Color.theme.accent)
-                    TextField("Enter carbs here...", value: $carb, format: .number)
+                    TextField("Enter carbs here...", value: $carb, formatter: quantityFormatter)
                         .padding()
                         .foregroundColor(Color.theme.accent)
                 }
@@ -63,16 +72,19 @@ struct Onboarding: View {
                     Text("Calories:")
                         .padding()
                         .foregroundColor(Color.theme.accent)
-                    TextField("Enter calories here...", value: $cal, format: .number)
+                    TextField("Enter calories here...", value: $cal, formatter: quantityFormatter)
                         .padding()
                         .foregroundColor(Color.theme.accent)
                 }
                 Button("Save and Close") {
-                    savedProtein = protein
-                    savedFat = fat
-                    savedCarb = carb
-                    savedCal = cal
+                    saveMacros()
                     appState.hasOnboarded = true
+                }
+                Section(header: Text("Saved Macros:")) {
+                    Text(String(savedProtein))
+                    Text(String(savedFat))
+                    Text(String(savedCarb))
+                    Text(String(savedCal))
                 }
             }
         }
