71c462f66f3f710adf2c18c1a23ea1a4b335d919
Nick Dazell <ndazell@g.hmc.edu>
2022-12-23 00:26:04 -0800

    built out daily log

--- CalCount/CustomViews/DailyLog.swift
+++ CalCount/CustomViews/DailyLog.swift
@@ -9,21 +9,61 @@ import SwiftUI
 
 struct DailyLog: View {
     @StateObject var viewRouter: ViewRouter
+    @State var foodName: String = ""
     var body: some View {
         ZStack {
             Color.theme.primary.ignoresSafeArea()
-            Text("Enter your foods below:")
-                .foregroundColor(Color.theme.accent)
-                .frame(width: 300, height: 500)
-                .position(x:205, y: 100)
-            Button("Return Home") {
-                viewRouter.currentPage = .homePage
+            VStack {
+                Text("Enter your foods below:")
+                    .foregroundColor(Color.theme.accent)
+                    .padding()
+                    //.frame(width: 300, height: 500)
+                    //.position(x:205, y: 100)
+                HStack {
+                    Text("Food:        ")
+                        .padding()
+                    TextField("Food Name...", text: $foodName)
+                        .padding()
+                }
+                HStack {
+                    Text("Protein(g):")
+                        .padding()
+                    TextField("Protein amount...", text: $foodName)
+                        .padding()
+                }
+                HStack {
+                    Text("Fat(g):       ")
+                        .padding()
+                    TextField("Fat amount...", text: $foodName)
+                        .padding()
+                }
+                HStack {
+                    Text("Carbs(g):  ")
+                        .padding()
+                    TextField("Carb amount...", text: $foodName)
+                        .padding()
+                }
+                HStack {
+                    Text("Calories:    ")
+                        .padding()
+                    TextField("Calorie amount...", text: $foodName)
+                        .padding()
+                }
+                Button("ENTER FOOD") {
+                    viewRouter.currentPage = .homePage
+                }
+                .padding()
+                Button("Return Home") {
+                    viewRouter.currentPage = .homePage
+                }
+                .padding()
+                //.position(x:205, y: 50)
+                Button("View History") {
+                    viewRouter.currentPage = .history
+                }
+                .padding()
+                //.position(x:205, y: 100)
             }
-            .position(x:205, y: 600)
-            Button("View History") {
-                viewRouter.currentPage = .history
-            }
-            .position(x:205, y: 700)
             
         }
             
